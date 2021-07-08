7caec3e064e86000f384c7abdd4926e78ab91706
matija2209 <matijazib@gmail.com>
2021-07-08 10:28:16 +0200

    quickfix

--- __main__.py
+++ __main__.py
@@ -19,30 +19,33 @@ def pages():
     sh.get_all_pages()
 
 def save_all_products():
-    sh = Shopify()
-    all_products = sh.get_all_products()
-    utils.save_to_csv(all_products,'products_data.csv')
+    countries = ['UK','EU']
+    for country in countries:
+        sh = Shopify(version=country)
+        all_products = sh.get_all_products()
+        utils.save_to_csv(all_products,f'products_data_{country}.csv')
 
 def update_oss_products():
-
-    sh = Shopify()
-    df = pd.read_csv('OOS notice on Shopify - data.csv')
-    url = f"{os.getenv('SPREADSHEET_URL')}/gviz/tq?tqx=out:csv&tq&"
-    r = requests.get(url)
-    data = r.text
-    with open("data.csv",mode="w") as file:
-        file.write(data)
-    df = pd.read_csv('data.csv')
-    json_file = df[['new_body_html','id']].to_json(orient="records")
-    data = json.loads(json_file)
-    for product in data:
-        id = product['id']
-        desc = product['new_body_html']
-        sh.update_product_desc(id,desc)
+    countries = ['UK','EU']
+    for country in countries:
+        sh = Shopify(version=country)
+        # df = pd.read_csv('OOS notice on Shopify - data.csv')
+        url = f"{os.getenv(f'SPREADSHEET_URL_{country}')}/gviz/tq?tqx=out:csv&tq&"
+        r = requests.get(url)
+        data = r.text
+        with open(f"data_{country}.csv",mode="w") as file:
+            file.write(data)
+        df = pd.read_csv(f'data_{country}.csv')
+        json_file = df[['new_body_html','id']].to_json(orient="records")
+        data = json.loads(json_file)
+        for product in data:
+            id = product['id']
+            desc = product['new_body_html']
+            sh.update_product_desc(id,desc)
 
 if __name__ == '__main__':
     update_oss_products()
-    # get_all_products()
+    # save_all_products()
     # pages()
     # delete_unfulfilled_orders()
     # answer = input("""What process do you want to run?\nInput one of the following numbers to begin one of the corresponding processes.\n-------\n
